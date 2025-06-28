b65d96856b0ca31a051c0a354bc552133ca53a6b
kmpaGitb <kmpaganesha@km.itb.ac.id>
2025-06-28 19:05:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-const URL = "https://script.google.com/macros/s/AKfycbzHYbEdcNDe2wL1EuI3elEQtjrMruKXZBwkDphG6Qyl3HOnOfh8jSUPxFOUhmgfsMMB/exec"; 
+const URL = "URL_WEB_APP_KAMU"; // Ganti dengan link web app kamu
 
 fetch(URL)
-  .then(res => res.json())
+  .then(response => response.json())
   .then(data => {
     const tbody = document.getElementById("tabel-alat");
-    data.forEach(barang => {
+    data.forEach((item, index) => {
       const row = `
         <tr>
-          <td>${barang.nama}</td>
-          <td>${barang.keterangan}</td>
-          <td>${barang.lokasi}</td>
-          <td>${barang.kuantitas}</td>
-          <td>${barang.jumlah_gudang}</td>
-        </tr>`;
+          <td>${item[0]}</td>
+          <td>${item[1]}</td>
+          <td>${item[2]}</td>
+          <td>${item[3]}</td>
+          <td>${item[4]}</td>
+          <td>${item[5]}</td>
+          <td>${item[6]}</td>
+          <td>${item[7]}</td>
+        </tr>
+      `;
       tbody.innerHTML += row;
     });
+  })
+  .catch(error => {
+    console.error("Gagal mengambil data:", error);
   });
